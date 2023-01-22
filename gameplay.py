935378c3e3213c3ce6fb3a2bc0314535746a38e7
liber09 <linda.bergsangel@hotmail.com>
2023-01-22 05:52:11 +0100

    Some coding to give player first card

--- gameplay.py
+++ gameplay.py
@@ -1,4 +1,5 @@
 import os
+import random
 from deck import Deck
 
 #Setup the game
@@ -8,6 +9,23 @@ def init_game():
     dealer_cards = []
     player_score = 0
     dealer_score = 0
+
+# Initial dealing for player and dealer
+    while len(player_cards) < 2:
+ 
+    # Randomly dealing a card
+        player_card = random.choice(deck)
+        player_cards.append(player_card)
+        deck.remove(player_card)
+    
+        # Updating the player score
+        player_score += player_card.card_value
+    
+        # In case both the cards are Ace, make the first ace value as 1 
+        if len(player_cards) == 2:
+            if player_cards[0].card_value == 11 and player_cards[1].card_value == 11:
+                player_cards[0].card_value = 1
+                player_score -= 10
 
     clear()
 
